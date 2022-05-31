30fc9f4d5111470b6876188e2c25002b718954a5
airsvn <tulic.peter77@gmail.com>
2022-05-31 12:54:21 +0200

    refactor

--- site/lib/newsletter.ts
+++ site/lib/newsletter.ts
@@ -1,7 +1,7 @@
 import { doc, setDoc, Timestamp } from 'firebase/firestore'
 import { db } from './firebase'
 
-export function subscribe(email) {
+export function subscribe(email: string) {
   return setDoc(doc(db, 'newsletter', email), {
     subscribed: Timestamp.now(),
   })
