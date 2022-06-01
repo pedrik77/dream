f29d82449f0a58fef7ba8efe0f94c8c95f18fa2c
Jana Blank <vojtkova.jana@gmail.com>
2022-06-01 14:22:29 +0200

    spelling

--- site/components/ui/Hero/Hero.tsx
+++ site/components/ui/Hero/Hero.tsx
@@ -20,7 +20,7 @@ const Hero: FC<HeroProps> = ({ headline, description }) => {
             <Link href="/">
               <a className="flex items-center text-accent-0 pt-3 font-bold hover:underline cursor-pointer w-max-content">
                 Read it here
-                <ArrowRight width="20" heigh="20" className="ml-1" />
+                <ArrowRight width="20" height="20" className="ml-1" />
               </a>
             </Link>
           </div>
