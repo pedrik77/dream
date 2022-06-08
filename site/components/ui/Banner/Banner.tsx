4260a2a674f67d2aa146939825d06d15b54fd211
Jana Blank <vojtkova.jana@gmail.com>
2022-06-08 05:28:02 +0200

    navbar/banner css unfinished

--- site/components/ui/Banner/Banner.tsx
+++ site/components/ui/Banner/Banner.tsx
@@ -4,22 +4,32 @@ import s from './Banner.module.css'
 
 const Banner = () => {
   return (
-    <div className="w-full h-32 flex">
+    <div className="w-screen h-[810px] flex relative">
       <Image
         className={s.img}
-        src="/assets/tesla.jpg"
+        src="/assets/tesla1_1440x910.jpg"
         alt="alt"
         layout="fill"
-        width={800}
-        height={600}
+        width={1440}
+        height={910}
         quality="100"
       />
-      <Button
-        aria-label="Join Now"
-        type="button"
-        className={s.button}
-        disabled={false}
-      />
+      <div className="container w-3/5 flex flex-col absolute top-20 left-20">
+        <h2 className="text-accent-0 font-extrabold text-7xl">
+          WIN A BRAND NEW CAR BLAH BLAH BLAH
+          <br></br>
+          <span className="text-secondary"> TESLA BLA</span>
+        </h2>
+
+        <div className="flex my-6">
+          <Button
+            aria-label="Join Now"
+            type="button"
+            className={s.button}
+            disabled={false}
+          />
+        </div>
+      </div>
     </div>
   )
 }
