74da367e7c4debcd2a01152673c87253e08fb1ec
airsvn <tulic.peter77@gmail.com>
2022-06-21 19:17:10 +0200

    products

--- site/lib/products.ts
+++ site/lib/products.ts
@@ -2,25 +2,37 @@ import {
   collection,
   deleteDoc,
   doc,
+  DocumentData,
   getDoc,
   onSnapshot,
   orderBy,
   query,
+  QueryDocumentSnapshot,
   setDoc,
 } from 'firebase/firestore'
 import { useEffect, useMemo, useState } from 'react'
 import { db } from './firebase'
 
-export interface Product {}
+export interface Product {
+  slug: string
+  title_1: string
+  title_2: string
+  short_desc: string
+  closing_date: Date
+  winner_announce_date: Date
+  gallery_id: string
+  long_desc: string
+  donation_entries: string
+}
 
 export async function getProduct(slug: string) {
   const productData = await getDoc(doc(db, 'products', slug))
 
-  return { slug: productData.id, ...productData.data() }
+  return transform(productData)
 }
 
-export async function setProduct(slug: string) {
-  return await setDoc(doc(db, 'products', slug), {})
+export async function setProduct({ slug, ...product }: Product) {
+  return await setDoc(doc(db, 'products', slug), product)
 }
 
 export async function deleteProduct(slug: string | string[]) {
@@ -31,7 +43,7 @@ export async function deleteProduct(slug: string | string[]) {
   )
 }
 
-export function useProduct() {
+export function useProducts() {
   const [products, setProducts] = useState<Product[]>([])
 
   useEffect(
@@ -39,14 +51,22 @@ export function useProduct() {
       onSnapshot(collection(db, 'products'), (querySnapshot) => {
         setProducts(
           // @ts-ignore
-          querySnapshot.docs.map((doc) => ({
-            slug: doc.id,
-            ...doc.data(),
-          }))
+          querySnapshot.docs.map(transform)
         )
       }),
     []
   )
 
-  return { products }
+  return products
+}
+
+function transform(doc: QueryDocumentSnapshot<DocumentData>) {
+  const { closing_date, winner_announce_date, ...data } = doc.data()
+
+  return {
+    ...data,
+    slug: doc.id,
+    closing_date: new Date(closing_date.seconds * 1000),
+    winner_announce_date: new Date(winner_announce_date.seconds * 1000),
+  }
 }
