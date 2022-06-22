b0b7a96120c26e3604197d3428d0cf1d06f88c44
Jana Blank <vojtkova.jana@gmail.com>
2022-06-22 17:41:43 +0200

    newsletter styling zle border

--- site/components/common/Footer/Newsletter.tsx
+++ site/components/common/Footer/Newsletter.tsx
@@ -1,14 +1,14 @@
 import { Button, Input } from '@components/ui'
 import { flash } from '@components/ui/FlashMessage'
 import { subscribe } from '@lib/newsletter'
-import {Checkbox} from '@mui/material'
+import { Checkbox } from '@mui/material'
 import React, { FormEventHandler, useState } from 'react'
 import s from './Newsletter.module.css'
 
 export default function Newsletter() {
   const [email, setEmail] = useState('')
   const [gdprChecked, setGdprChecked] = useState(false)
-  
+
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
     console.log('submit', e)
@@ -27,27 +27,33 @@ export default function Newsletter() {
 
   return (
     <>
-      <h3>SUBSCRIBE TOU OUR NEEEEWS</h3>
+      <h3>PRIHLASENIE DO ODBERU NOVINIEK</h3>
       <form onSubmit={handleSubmit}>
-        <fieldset className={s.root}>
+        <fieldset className={s.main}>
           <Input
             type="text"
             value={email}
-            placeholder="Newsletter"
+            placeholder="Zadajte email"
             onChange={setEmail}
             className={s.input}
           />
-          
+
           <Button className={s.button} type="submit">
             Prihlásiť
           </Button>
         </fieldset>
-        <Checkbox checked={gdprChecked} onChange={e => setGdprChecked(!!e.target.checked)} />
-      <p className={s.text}>
-        Wafer sweet bonbon dessert cupcake. Muffin apple pie candy oat cake
-        liquorice brownie tart. Tiramisu chocolate cake apple pie muffin
-        chocolate bar gummi bears sugar plum.
-      </p>
+        <fieldset className={s.gdpr}>
+          <Checkbox
+            className={s.checkbox}
+            checked={gdprChecked}
+            onChange={(e) => setGdprChecked(!!e.target.checked)}
+          />
+          <p className={s.text}>
+            Wafer sweet bonbon dessert cupcake. Muffin apple pie candy oat cake
+            liquorice brownie tart. Tiramisu chocolate cake apple pie muffin
+            chocolate bar gummi bears sugar plum.
+          </p>
+        </fieldset>
       </form>
     </>
   )
