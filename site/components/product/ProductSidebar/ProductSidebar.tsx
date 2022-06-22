16475e8d70103d67df798ce460278b8ae96c029c
Jana Blank <vojtkova.jana@gmail.com>
2022-06-22 21:21:02 +0200

    product page css edits

--- site/components/product/ProductSidebar/ProductSidebar.tsx
+++ site/components/product/ProductSidebar/ProductSidebar.tsx
@@ -42,45 +42,26 @@ const ProductSidebar: FC<ProductSidebarProps> = ({ product, className }) => {
   }
 
   return (
-    <div className={className}>
+    <div className={s.sidebar}>
+      <h5 className={s.category}>Kategoria</h5>
       <ProductTag name={product.name} />
-
+      <h4 className={s.subtitle}>
+        Jelly dessert icing caramels biscuit tootsie.
+      </h4>
       <Text
         className="pb-4 break-words w-full max-w-xl"
         html={product.descriptionHtml || product.description}
       />
-      <div className="flex flex-row justify-between items-center">
-        <Rating value={4} />
-        <div className="pr-1 font-medium text-sm">36 reviews</div>
+      <div className={s.info}>
+        <div>
+          <span className={s.infoTitle}>Closes</span>
+          <span>12.12.2022</span>
+        </div>
+        <div>
+          <span className={s.infoTitle}>Winner Announcement</span>
+          <span>20.12.2022</span>
+        </div>
       </div>
-      <div>
-        {process.env.COMMERCE_CART_ENABLED && (
-          <Button
-            aria-label="Add to Cart"
-            type="button"
-            className={s.button}
-            onClick={addToCart}
-            loading={loading}
-            disabled={variant?.availableForSale === false}
-          >
-            {variant?.availableForSale === false
-              ? 'Not Available'
-              : 'Add To Cart'}
-          </Button>
-        )}
-      </div>
-      <div className="mt-6">
-        <Collapse title="Care">
-          This is a limited edition production run. Printing starts when the
-          drop ends.
-        </Collapse>
-        <Collapse title="Details">
-          This is a limited edition production run. Printing starts when the
-          drop ends. Reminder: Bad Boys For Life. Shipping may take 10+ days due
-          to COVID-19.
-        </Collapse>
-      </div>
-
       {/* TODO Toto ten button neviem */}
       <div className="flex justify-center">
         <Button type="button" className={(s.button, 'my-5')}>
