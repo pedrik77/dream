56134a76a95e97bed8b197107890ad06362b358e
Jana Blank <vojtkova.jana@gmail.com>
2022-06-29 11:26:09 +0200

    mobile - account info layout change

--- site/components/auth/AccountLayout.tsx
+++ site/components/auth/AccountLayout.tsx
@@ -21,25 +21,27 @@ const AccountLayout: React.FC<{
 
   return (
     <Container className="pt-4 mt-8">
-      <div className="flex gap-3">
-        <div className="w-1/3 flex flex-col gap-4 mx-4 border-r-[1px] border-opacity-70 border-primary items-center text-2xl uppercase text-center">
+      <div className="flex flex-col lg:flex-row gap-3">
+        <div className="lg:w-1/3 flex flex-col gap-4 pr-4 pb-4 mx-2 g:mx-4 border-b-[1px] lg:border-r-[1px] border-opacity-70 border-primary items-center justify-center text-xl lg:text-2xl uppercase text-center">
           <div className="flex justify-center align-center h-32 w-32">
             <Image height={200} width={200} alt="avatar" src="/icon.png" />
           </div>
-          {tuples.map(([href, label]) => (
-            <Link key={href} href={'/' + href}>
-              <a
-                title={label}
-                className={
-                  current === href
-                    ? 'border-b-2 border-primary border-opacity-70'
-                    : 'text-primary hover:border-b-2 border-secondary'
-                }
-              >
-                {label}
-              </a>
-            </Link>
-          ))}
+          <div className="flex flex-row lg:flex-col gap-4 justify-center items-center">
+            {tuples.map(([href, label]) => (
+              <Link key={href} href={'/' + href}>
+                <a
+                  title={label}
+                  className={
+                    current === href
+                      ? 'border-b-2 border-primary border-opacity-70'
+                      : 'text-primary hover:border-b-2 border-secondary'
+                  }
+                >
+                  {label}
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
         <div className="">{children}</div>
       </div>
