f606dbbcfd21bf1543329a84005bfacd1584195a
Jana Blank <vojtkova.jana@gmail.com>
2022-07-01 16:03:52 +0200

    stepper design

--- site/pages/cart.tsx
+++ site/pages/cart.tsx
@@ -4,11 +4,11 @@ import Stepper from '@components/cart/Stepper'
 
 export default function Cart() {
   return (
-    <Container className="grid lg:grid-cols-12 pt-4 gap-20">
-      <Text variant="heading" className="col-span-6">
-        Košík
-      </Text>
-      <Stepper className="col-span-6" />
+    <Container className="grid lg:grid-cols-3 pt-4 gap-20">
+      <Text variant="heading">Košík</Text>
+      <Container clean>
+        <Stepper />
+      </Container>
     </Container>
   )
 }
