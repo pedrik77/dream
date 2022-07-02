feaecb28dcf5e6c5b70c76035e1423d87c7e9f35
Jana Blank <vojtkova.jana@gmail.com>
2022-07-02 14:01:49 +0200

    staging cart pge layout 2

--- site/components/cart/CartItem/CartItem.tsx
+++ site/components/cart/CartItem/CartItem.tsx
@@ -1,18 +1,35 @@
-import { Container, Text } from '@components/ui'
-import { CartItem as ICartItem } from '@lib/shop'
+import { Cross } from '@components/icons'
+import { Button, Container, Text } from '@components/ui'
+import { CartItem as ICartItem, useShop } from '@lib/shop'
 import Image from 'next/image'
 import React from 'react'
 
 export default function CartItem({ product, ticketCount, price }: ICartItem) {
+  const { removeFromCart } = useShop()
+
+  const handleRemove = () => removeFromCart(product.slug)
+
   return (
-    <Container clean className="flex center items-stretch">
-      <Image height={195} width={300} alt="productImage" src={product.image} />
-      <div className="flex gap-4 center items-stretch">
+    <Container className="flex center items-stretch my-4">
+      <Image
+        height={195}
+        width={300}
+        alt="productImage"
+        src={product.image}
+        className="rounded-md"
+      />
+      <div className="flex flex-col gap-4 center items-stretch">
         <Text variant="heading">{ticketCount}</Text>
         <Text>{product.title_1}</Text>
         <Text>{product.title_2}</Text>
       </div>
+
       <Text variant="heading">{price}</Text>
+      <div className="flex center">
+        <Button onClick={handleRemove}>
+          <Cross />
+        </Button>
+      </div>
     </Container>
   )
 }
