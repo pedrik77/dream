9b3d6ea26d66d24a1e491d6c42c23f35501e4357
airsvn <tulic.peter77@gmail.com>
2022-07-02 14:01:08 +0200

    c

--- site/components/cart/Information/Information.tsx
+++ site/components/cart/Information/Information.tsx
@@ -1,16 +1,54 @@
-import { Text } from '@components/ui'
+import { Input, Text } from '@components/ui'
 import { useUser } from '@lib/auth'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Information() {
-  const { user } = useUser()
+  const { user, customer } = useUser()
+
+  const [fullname, setFullname] = useState(customer.fullname)
+  const [email, setEmail] = useState(user?.email || '@')
+  const [phone, setPhone] = useState(customer.phone)
+  const [street, setStreet] = useState(customer.address.street)
+  const [city, setCity] = useState(customer.address.city)
+  const [zip, setZip] = useState(customer.address.zip)
+  const [country, setCountry] = useState(customer.address.country)
 
   return (
     <div>
       <Text variant="sectionHeading" className="my-4">
         Informacie
       </Text>
-      <div></div>
+      <div>
+        <fieldset className="flex">
+          <label>
+            Full name{' '}
+            <Input
+              type="text"
+              value={fullname}
+              placeholder="Full name"
+              onChange={setFullname}
+            />
+          </label>
+          <label>
+            Email{' '}
+            <Input
+              type="email"
+              value={email}
+              placeholder="Email"
+              onChange={setEmail}
+            />
+          </label>
+          <label>
+            Phone{' '}
+            <Input
+              type="tel"
+              value={phone}
+              placeholder="Phone"
+              onChange={setPhone}
+            />
+          </label>
+        </fieldset>
+      </div>
     </div>
   )
 }
