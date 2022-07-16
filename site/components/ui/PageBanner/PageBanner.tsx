82644c11ecceee85623c40a569c754938dac8f45
Jana Blank <vojtkova.jana@gmail.com>
2022-07-16 18:47:43 +0200

    Banner component created

---
+++ site/components/ui/PageBanner/PageBanner.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import s from './PageBanner.module.css'
+
+const PageBanner: React.FC<{
+  primaryTitle: string
+  secondaryTitle: string
+  img: string
+}> = ({ primaryTitle, secondaryTitle, img }) => {
+  return (
+    <div className={s.bgOverlay}>
+      <div className={s.imgContainer}>
+        <Image
+          src={img}
+          alt="alt"
+          width="1440"
+          height="810"
+          layout="responsive"
+          quality="100"
+        />
+      </div>
+      <div className={s.textContainer}>
+        <h2 className={s.h2}>
+          {primaryTitle}
+          <br />
+          <span className={s.span}>{secondaryTitle}</span>
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default PageBanner
--- site/pages/products/index.tsx
+++ site/pages/products/index.tsx
@@ -1,6 +1,7 @@
 import { Layout } from '@components/common'
 import { ProductCard } from '@components/product'
 import { Container } from '@components/ui'
+import PageBanner from '@components/ui/PageBanner'
 import { useProducts } from '@lib/products'
 import { GetServerSideProps } from 'next'
 import React from 'react'
@@ -13,11 +14,18 @@ export default function Products({ category = '' }: ProductsPageProps) {
   const products = useProducts(category)
 
   return (
-    <Container>
-      {!products.length && 'Žiadne produkty v kategórii'}
-      {products.map((product) => (
-        <ProductCard key={product.slug} product={product} />
-      ))}
+    <Container clean>
+      <PageBanner
+        primaryTitle="Lorem Ipsum"
+        secondaryTitle="Chocolate Cake"
+        img="/assets/tesla1_1440x810.jpg"
+      />
+      <Container>
+        {!products.length && 'Žiadne produkty v kategórii'}
+        {products.map((product) => (
+          <ProductCard key={product.slug} product={product} />
+        ))}
+      </Container>
     </Container>
   )
 }
