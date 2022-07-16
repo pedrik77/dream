a0693951b6fc0d3d0026c0d8c995c284a4bc3826
janablank <75183484+janablank@users.noreply.github.com>
2022-07-16 18:56:33 +0200

    Feature/94 page banner create new banner tyoe (#95)
    * Banner component created
    
    * Banner layout and styling
    
    * Remove test code

--- site/pages/products/index.tsx
+++ site/pages/products/index.tsx
@@ -1,6 +1,7 @@
 import { Layout } from '@components/common'
 import { ProductCard } from '@components/product'
 import { Container } from '@components/ui'
+import PageBanner from '@components/ui/PageBanner'
 import { useProducts } from '@lib/products'
 import { GetServerSideProps } from 'next'
 import React from 'react'
