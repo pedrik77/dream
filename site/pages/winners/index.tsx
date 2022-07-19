a859e4bc7557e4be703cefc99608602ceda29ea1
Jana Blank <vojtkova.jana@gmail.com>
2022-07-19 21:34:59 +0200

    NEviem

--- site/pages/winners/index.tsx
+++ site/pages/winners/index.tsx
@@ -8,6 +8,7 @@ import React, { Fragment, useMemo } from 'react'
 import { inputDateFormat } from '@lib/date'
 import { Product, useProducts } from '@lib/products'
 import { ProductCard } from '@components/product'
+import PageBanner from '@components/ui/PageBanner'
 
 const MONTHS = [
   'január',
@@ -87,72 +88,79 @@ export default function Winners({ date = '' }: WinnersPageProps) {
     'flex flex-row lg:flex-col gap-4 justify-center items-center text-lg md:text-xl'
 
   return (
-    <Container className="pt-4 mt-0 md:mt-8">
-      <div className="grid grid-cols-12 lg:flex-row gap-3 lg:gap-6">
-        <div className="col-span-2 flex flex-col gap-4 pr-4 pb-8 mx-2 g:mx-4 lg:border-r-[1px] border-opacity-70 border-primary items-center justify-center md:justify-start text-lg lg:text-2xl uppercase text-center">
-          <Text variant="pageHeading">
-            <Link href="/winners">
-              <a>Víťazi</a>
-            </Link>
-          </Text>
-          <div className={treeClass}>
-            {Object.entries(yearTree).map(([year, monthTree]) => (
-              <Fragment key={year}>
-                <Link href={`/winners?date=${year}`}>
-                  <a
-                    title={year}
-                    className={
-                      currentYear === year
-                        ? 'border-b-2 border-primary border-opacity-70'
-                        : 'text-primary hover:border-b-2 border-secondary'
-                    }
-                  >
-                    {year}
-                  </a>
-                </Link>
-
-                {currentYear === year && (
-                  <div className={treeClass + ' pl-5 text-sm'}>
-                    {Object.entries(monthTree).map(([month, winners]) => (
-                      <Link
-                        href={`/winners?date=${year}-${month}`}
-                        key={`${month}/${year}`}
-                      >
-                        <a
-                          title={month}
-                          className={
-                            currentMonth === month
-                              ? 'border-b-2 border-primary border-opacity-70'
-                              : 'text-accent-6 text-sm hover:text-primary'
-                          }
-                        >
-                          {monthName(month)}
-                        </a>
-                      </Link>
-                    ))}
-                  </div>
-                )}
-              </Fragment>
-            ))}
-          </div>
-        </div>
-        <div className="col-span-10">
-          {date && (
+    <Container clean>
+      <PageBanner
+        primaryTitle="Lorem ipsum doleres chocolates"
+        secondaryTitle="Neviem uz boha co"
+        img="/assets/tesla1_1440x810.jpg"
+      />
+      <Container className="py-8 mt-0 md:mt-8 items-center justify-center">
+        <div className="flex flex-col gap-3 lg:gap-6 max-w-lg md:max-w-2xl mx-auto lg:max-w-6xl items-center justify-center">
+          <div className="flex flex-col gap-4 pr-4 pb-8 mx-2 g:mx-4 lg:border-r-[1px] border-opacity-70 border-primary items-center justify-center md:justify-start text-lg lg:text-2xl uppercase text-center">
             <Text variant="pageHeading">
-              {currentMonthName} {currentYear}
+              <Link href="/winners">
+                <a>Víťazi</a>
+              </Link>
             </Text>
-          )}
-          <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 my-8 py-2 gap-4 md:gap-8">
-            {products.map((product) => (
-              <ProductCard
-                variant="simple"
-                key={product.slug}
-                product={product}
-              />
-            ))}
+            <div className={treeClass}>
+              {Object.entries(yearTree).map(([year, monthTree]) => (
+                <Fragment key={year}>
+                  <Link href={`/winners?date=${year}`}>
+                    <a
+                      title={year}
+                      className={
+                        currentYear === year
+                          ? 'border-b-2 border-primary border-opacity-70'
+                          : 'text-primary hover:border-b-2 border-secondary'
+                      }
+                    >
+                      {year}
+                    </a>
+                  </Link>
+
+                  {currentYear === year && (
+                    <div className={treeClass + ' pl-5 text-sm'}>
+                      {Object.entries(monthTree).map(([month, winners]) => (
+                        <Link
+                          href={`/winners?date=${year}-${month}`}
+                          key={`${month}/${year}`}
+                        >
+                          <a
+                            title={month}
+                            className={
+                              currentMonth === month
+                                ? 'border-b-2 border-primary border-opacity-70'
+                                : 'text-accent-6 text-sm hover:text-primary'
+                            }
+                          >
+                            {monthName(month)}
+                          </a>
+                        </Link>
+                      ))}
+                    </div>
+                  )}
+                </Fragment>
+              ))}
+            </div>
+          </div>
+          <div className="">
+            {date && (
+              <Text variant="pageHeading">
+                {currentMonthName} {currentYear}
+              </Text>
+            )}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-8 py-2 gap-4 md:gap-8">
+              {products.map((product) => (
+                <ProductCard
+                  variant="simple"
+                  key={product.slug}
+                  product={product}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </Container>
     </Container>
   )
 }
