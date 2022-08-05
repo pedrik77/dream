d8bb62f31db92c3d6d86bda9938887ac0a65a6d2
Jana Blank <vojtkova.jana@gmail.com>
2022-08-05 13:04:51 +0200

    Succes button color and text

--- site/lib/alerts.ts
+++ site/lib/alerts.ts
@@ -4,6 +4,7 @@ export const confirm = async (title: string) => {
   const { isConfirmed } = await Swal.fire({
     title,
     confirmButtonText: 'Áno',
+    confirmButtonColor: 'var(--text-primary)',
     showCancelButton: true,
     cancelButtonText: 'Zrušiť',
   })
