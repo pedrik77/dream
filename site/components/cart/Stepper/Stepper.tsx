bf776d5b96258c1fc84592ad6b128d590d741476
Jana Blank <vojtkova.jana@gmail.com>
2022-08-05 15:29:21 +0200

    Stepper Add sx

--- site/components/cart/Stepper/Stepper.tsx
+++ site/components/cart/Stepper/Stepper.tsx
@@ -17,6 +17,7 @@ export default function Stepper({
       className={className}
       activeStep={steps.indexOf(activeStep)}
       alternativeLabel
+      sx={}
     >
       {steps.map((title) => (
         <Step key={title}>
