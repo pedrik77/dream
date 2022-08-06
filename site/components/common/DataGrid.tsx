0ff64f476208031540c211090c014bb45fefad93
airsvn <tulic.peter77@gmail.com>
2022-08-06 05:46:06 +0200

    betta

--- site/components/common/DataGrid.tsx
+++ site/components/common/DataGrid.tsx
@@ -3,15 +3,18 @@ import {
   DataGrid as MuiDG,
   DataGridProps,
   GridColDef,
+  GridRowSpacing,
   GridValidRowModel,
   skSK,
 } from '@mui/x-data-grid'
 
-interface ICol extends React.FC<GridColDef> {}
-
 interface AdditionalProps<R> {
   rowIdKey: keyof R
   className?: string
+  wrapperClassName?: string
+  rowClassName?: string
+  cellClassName?: string
+  rowSpacing?: GridRowSpacing
   children?: any
 }
 
@@ -25,33 +28,44 @@ interface DataGridComponent {
 }
 
 export const DataGrid: DataGridComponent = ({
-  className = '',
   rowIdKey,
+  className = '',
+  wrapperClassName = '',
+  rowClassName = 'cursor-pointer',
+  cellClassName = '',
+  rowSpacing,
   children,
   ...props
 }) => {
   const columns = (Array.isArray(children) ? children : [children]).map(
-    ({ props }) => ({
-      ...props,
-      sortable: !!props.sortable ? props.sortable : false,
-    })
+    ({ props }) => modifyColProps(props)
   )
 
   return (
-    <div className={`w-full h-[650px] my-4 ${className}`}>
+    <div className={`w-full h-[650px] my-4 ${wrapperClassName}`}>
       <MuiDG
-        className="border-none font-[Raleway] text-base"
         pageSize={10}
         rowsPerPageOptions={[10]}
         getRowId={(row) => row[rowIdKey]}
-        getRowClassName={() => 'cursor-pointer'}
+        getRowClassName={() => rowClassName}
+        getCellClassName={() => cellClassName}
+        getRowSpacing={rowSpacing ? () => rowSpacing : undefined}
         disableSelectionOnClick
         localeText={skSK.components.MuiDataGrid.defaultProps.localeText}
         {...props}
+        className={`border-none font-[Raleway] text-base ${className}`}
         columns={columns}
       />
     </div>
   )
 }
 
-export const Col: ICol = () => null
+export const Col: React.FC<GridColDef> = () => null
+
+const modifyColProps = ({
+  sortable = false,
+  ...props
+}: GridColDef): GridColDef => ({
+  ...props,
+  sortable,
+})
