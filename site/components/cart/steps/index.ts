6023a43414d81c4989d6ad80cf1c52430b9bcc07
airsvn <tulic.peter77@gmail.com>
2022-08-10 15:23:39 +0200

    stepiky

---
+++ site/components/cart/steps/index.ts
@@ -0,0 +1,14 @@
+import React from 'react'
+import Done from './Done'
+import Information from './Information'
+import Payment from './Payment'
+import Products from './Products'
+
+const steps = [
+  ['Košík', Products],
+  ['Informácie', Information],
+  ['Platba', Payment],
+  ['Hotovo', Done],
+]
+
+export default steps
