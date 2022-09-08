443d0efd92530a9f6d8f1a9369e0061ed0ef1454
airsvn <tulic.peter77@gmail.com>
2022-09-08 18:49:24 +0200

    scroll disable

---
+++ site/lib/hooks/useScrollDIsable.ts
@@ -0,0 +1,27 @@
+import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'
+import { useCallback, useEffect } from 'react'
+
+export const useScrollDisable = (
+  target: HTMLElement | Element,
+  onClose = () => {}
+) => {
+  const handleKey = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        return onClose()
+      }
+    },
+    [onClose]
+  )
+
+  useEffect(() => {
+    if (target) {
+      disableBodyScroll(target, { reserveScrollBarGap: true })
+      window.addEventListener('keydown', handleKey)
+    }
+    return () => {
+      clearAllBodyScrollLocks()
+      window.removeEventListener('keydown', handleKey)
+    }
+  }, [handleKey, target])
+}
