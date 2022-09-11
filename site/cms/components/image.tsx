670564dbfb18c489d30a1d67527dee43e7911890
airsvn <tulic.peter77@gmail.com>
2022-09-11 04:05:36 +0200

    ta

--- site/cms/components/image.tsx
+++ site/cms/components/image.tsx
@@ -25,21 +25,6 @@ const config: ComponentConfig<ImageProps> = {
       'https://firebasestorage.googleapis.com/v0/b/dream-38748.appspot.com/o/cms%2Fpage_banners%2F09d422b7-a49d-4dd6-9ffc-38a1c3e1b382?alt=media&token=20db2e45-008d-4796-9d97-ccebb22a7f4b',
 
       ({ value, onChange, component }) => {
-        const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-          if (!e.target.files?.[0]) return
-
-          const file = e.target.files[0]
-          const reader = new FileReader()
-
-          reader.onload = (e) => {
-            if (!e.target?.result) return
-            onChange(e.target.result as string)
-          }
-
-          reader.readAsDataURL(file)
-          uploadFile('cms/' + uuid4(), file).then((src) => onChange(src))
-        }
-
         const { width, height } = component ?? {}
         const imagePreview = width && height ? { width, height } : undefined
 
@@ -47,6 +32,9 @@ const config: ComponentConfig<ImageProps> = {
           <Input
             type="file"
             value={value}
+            onFile={(file) =>
+              uploadFile('cms/' + uuid4(), file).then((src) => onChange(src))
+            }
             onChange={console.log}
             imagePreview={imagePreview}
           />
