2bb041020a6ca6822aea9c97e76bb784d477d0f2
airsvn <tulic.peter77@gmail.com>
2022-09-28 12:58:42 +0200

    fix branch

--- components/common/SocialSection/SocialSection.tsx
+++ components/common/SocialSection/SocialSection.tsx
@@ -19,28 +19,26 @@ const SocialSection = ({ title, links }: SocialSectionProps) => {
     <div className="flex flex-col gap-3">
       <h3 className="font-bold">{title}</h3>
       <div className="flex gap-3 lg:justify-end">
-        {links.map((link) => (
-          <a
-            key={link}
-            className={s.link}
-            target="_blank"
-            rel="noreferrer"
-            href={link}
-          >
-            <div>
-              {/*
+        {links.map((link) => {
+          const social = SOCIAL_OPTIONS.find((social) => link.includes(social))
+
+          return (
+            <a
+              key={link}
+              className={s.link}
+              target="_blank"
+              rel="noreferrer"
+              href={link}
+            >
+              <div>
+                {/*
                eslint-disable-next-line @next/next/no-img-element
              */}
-              <img
-                src={`/${SOCIAL_OPTIONS.find((social) =>
-                  link.includes(social)
-                )}.png`}
-                alt="icon"
-                className={s.icon}
-              />
-            </div>
-          </a>
-        ))}
+                <img src={`/${social}.png`} alt={social} className={s.icon} />
+              </div>
+            </a>
+          )
+        })}
       </div>
     </div>
   )
