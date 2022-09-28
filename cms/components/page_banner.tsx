5f5c8fe6b8da1afc15c41e8e682b956fccd8384b
Jana Blank <vojtkova.jana@gmail.com>
2022-09-28 20:13:09 +0200

    Page banner trans

--- cms/components/page_banner.tsx
+++ cms/components/page_banner.tsx
@@ -10,7 +10,7 @@ const config: ComponentConfig<PageBannerProps> = {
   ),
   valuesDefinition: {
     img: [
-      'Image',
+      ['cms.labels.images.title', 'cms.labels.images.pageBanner'],
       '/assets/winners_banner.jpg',
       // @ts-ignore
       getImageInput({ getPath: () => 'cms/page_banner' }),
