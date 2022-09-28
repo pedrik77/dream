dccb7da2b5eaf93ab06ac2f109fde61e0adbba98
Jana Blank <vojtkova.jana@gmail.com>
2022-09-28 19:06:26 +0200

    emhhehehe

---
+++ packages/sfcc/dist/index.js
@@ -0,0 +1,6 @@
+import { getCommerceProvider, useCommerce as useCoreCommerce } from '@vercel/commerce';
+import { sfccProvider } from './provider';
+export { sfccProvider };
+export const CommerceProvider = getCommerceProvider(sfccProvider);
+export const useCommerce = ()=>useCoreCommerce()
+;
