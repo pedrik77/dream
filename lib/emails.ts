d706a3aa351f8dd6c3f95a834e610dcc455cc02b
airsvn <tulic.peter77@gmail.com>
2022-10-07 14:54:26 +0200

    tak

--- lib/emails.ts
+++ lib/emails.ts
@@ -1,8 +1,22 @@
 import { api } from './api'
 
+export const SIGN_UP_CMS_ID = 'email__sign-up'
+export const RESET_PASSWORD_CMS_ID = 'email__reset-password'
 export const ORDER_CREATED_CMS_ID = 'email__order-created'
-export const VERIFICATION_CMS_ID = 'email__verification'
+export const UNPAID_ORDER_CREATED_CMS_ID = 'email__unpaid-order-created'
 export const PRODUCT_CLOSE_CMS_ID = 'email__product-close'
+export const WINNER_ANNOUNCEMENT_CMS_ID = 'email__winner-announcement'
+export const VERIFICATION_CMS_ID = 'email__verification'
+
+export const templates = {
+  [SIGN_UP_CMS_ID]: ['name'],
+  [RESET_PASSWORD_CMS_ID]: ['name'],
+  [ORDER_CREATED_CMS_ID]: ['name'],
+  [UNPAID_ORDER_CREATED_CMS_ID]: ['name'],
+  [PRODUCT_CLOSE_CMS_ID]: ['name'],
+  [WINNER_ANNOUNCEMENT_CMS_ID]: ['name'],
+  [VERIFICATION_CMS_ID]: ['name'],
+}
 
 export async function sendEmailVerificationEmail(email: string) {
   const { data } = await api.post('/email/verification', { email })
