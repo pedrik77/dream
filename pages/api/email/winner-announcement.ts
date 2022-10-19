19bf724c64621da467b625cbde9433681b22a5c1
airsvn <tulic.peter77@gmail.com>
2022-10-19 09:52:19 +0200

    e

--- pages/api/email/winner-announcement.ts
+++ pages/api/email/winner-announcement.ts
@@ -1,7 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { sendMail } from '@lib/mailer'
 import { getSingleComponent } from '@lib/cms'
-import { processPlaceholders, WINNER_ANNOUNCEMENT_CMS_ID } from '@lib/emails'
+import {
+  getActionButton,
+  processPlaceholders,
+  WINNER_ANNOUNCEMENT_CMS_ID,
+} from '@lib/emails'
+import { getProduct } from '@lib/products'
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,13 +14,25 @@ export default async function handler(
 ) {
   if (req.method !== 'POST') return res.status(405).send('Method not allowed')
 
+  const { productSlug } = req.body
+
+  if (!productSlug) return res.status(400).send('Missing product slug')
+
+  const product = await getProduct(productSlug, { withCmsBlocks: false })
+
   const template = await getSingleComponent(WINNER_ANNOUNCEMENT_CMS_ID)
 
   await sendMail(
     { address: 'tulic.peter77@gmail.com', name: 'meh' },
     template.value.subject,
     processPlaceholders(template.value.template, {
-      name: 'testovy mail, tunak text dajak jeeeeb',
+      // firstname: customer.firstname,
+      // lastname: customer.lastname,
+      // email: customer.email,
+      action: getActionButton(
+        process.env.NEXT_PUBLIC_URL + `/products/${product.slug}`,
+        template.value.actionButtonText
+      ),
     })
   )
 
